6b1e3031bc323d2155ca018c251d6f840eb0966d
NgocQuynh15 <22004013@st.vlute.edu.vn>
2025-10-02 19:03:25 +0700

    c

--- php/app/views/quan_ly/trangchu.php
+++ php/app/views/quan_ly/trangchu.php
@@ -88,7 +88,7 @@
                                 <p class="text-xs text-gray-500">Tài khoản cá nhân</p>
                             </div>
                         </div>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Trang cá nhân</a>
+                        <a href="trangchu.php?page=../moi_gioi/ql_hoso_canhan" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Trang cá nhân</a>
                         <a href="../../models/auth/xuly_dangxuat.php" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100">Đăng xuất</a>
                     </div>
                 </div>
